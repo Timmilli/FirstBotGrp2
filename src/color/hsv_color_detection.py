519307364660166a6428b6ce137c5d5ac9ad77ba
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-09-30 16:05:51 +0200

    hsv color detection and color finding, codes are correct, might work already

---
+++ src/color/hsv_color_detection.py
@@ -0,0 +1,141 @@
+import numpy as np
+import cv2
+import pypot.dynamixel
+
+USE_ON_COMPUT = True
+
+ports = pypot.dynamixel.get_available_ports()
+if not ports:
+    MOTOR_USED = False
+    print("Motor not used.")
+else:
+    MOTOR_USED = True
+    print("Motor used.")
+
+if MOTOR_USED:
+    dxl_io = pypot.dynamixel.DxlIO(ports[0])
+    dxl_io.set_wheel_mode([1])
+    STANDARD_SPEED = 360
+
+
+def coord_is_in_left(coord):
+    x, y = coord
+    if x < width / 3:
+        return True
+    return False
+
+
+def coord_is_in_center(coord):
+    x, y = coord
+    if width / 3 <= x and x <= width * 2 / 3:
+        return True
+    return False
+
+
+def coord_is_in_right(coord):
+    x, y = coord
+    if width * 2 / 3 < x:
+        return True
+    return False
+
+
+video_capture = cv2.VideoCapture(0)
+
+width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+print(f"width:{width}; height:{height}; fps:{fps}")
+
+# Coded in HSV
+boundaries = [
+    ([0, 30, 125], [30, 100, 255]),  # A yellow tape
+    ([100, 90, 170], [120, 170, 230]),  # A blue tape
+    ([170, 100, 100], [180, 255, 255]),  # A red tape
+    ([0, 50, 70], [20, 140, 150])  # A maroon tape
+]
+
+current_color = 0
+
+
+def next_color():
+    global current_color
+    if current_color < len(boundaries)-1:
+        current_color += 1
+    else:
+        current_color = 0
+    return current_color
+
+
+lower, upper = boundaries[next_color()]
+lower = np.array(lower, dtype="uint8")
+upper = np.array(upper, dtype="uint8")
+
+while True:
+    result, frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    frame = frame[0:height, int(width/2)-5:int(width/2)+5]
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Mask and output for color to be followed
+    mask = cv2.inRange(hsv_frame, lower, upper)
+    if USE_ON_COMPUT:
+        output = cv2.bitwise_and(hsv_frame, hsv_frame, mask=mask)
+    # Find all pixels detected in the mask
+    coords = cv2.findNonZero(mask)
+    # In case if nothing is detected
+    nb_left = 0
+    nb_center = 0
+    nb_right = 0
+    if coords is not None:
+        for coord in coords:
+            # If the coordinate is in the first (left) third
+            if coord_is_in_left(coord[0]):
+                # Show the coordinate as red
+                if USE_ON_COMPUT:
+                    output = cv2.circle(output, coord[0], radius=0,
+                                        color=(0, 0, 255), thickness=-1)
+                # Add the pixel to the left counter
+                nb_left += 1
+            elif coord_is_in_center(coord[0]):
+                if USE_ON_COMPUT:
+                    output = cv2.circle(output, coord[0], radius=0,
+                                        color=(0, 255, 0), thickness=-1)
+                nb_center += 1
+            elif coord_is_in_right(coord[0]):
+                if USE_ON_COMPUT:
+                    output = cv2.circle(output, coord[0], radius=0,
+                                        color=(255, 0, 0), thickness=-1)
+                nb_right += 1
+    else:
+        coords = [0]
+    # Percentages of mask pixels in a zone of the image
+    print(nb_left/len(coords), nb_center/len(coords), nb_right/len(coords))
+    if MOTOR_USED:
+        dxl_io.set_moving_speed(
+            {2: -(STANDARD_SPEED - nb_left/len(coords)*STANDARD_SPEED)})  # Degrees / s
+        dxl_io.set_moving_speed(
+            {1: STANDARD_SPEED - nb_right/len(coords)*STANDARD_SPEED})  # Degrees / s
+
+    if USE_ON_COMPUT:
+        # Visual line, not necessary for computing
+        cv2.line(output, (int(width/3), 0),
+                 (int(width/3), height), (255, 0, 0), 2)
+        cv2.line(output, (int(width*2/3), 0),
+                 (int(width*2/3), height), (255, 0, 0), 2)
+
+        # Showing images
+        cv2.imshow("images", np.hstack([frame, output]))
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+video_capture.release()
+cv2.destroyAllWindows()
+if MOTOR_USED:
+    dxl_io.set_moving_speed(
+        {1: 0})  # Degrees / s
+    dxl_io.set_moving_speed(
+        {2: 0})  # Degrees / s
