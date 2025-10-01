c18438299b52e66667b06c5b0dc1bc1714f2a3ed
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-10-01 15:29:48 +0200

    working program with old meca

---
+++ src/color/old_meca/blue_following_rasp.py
@@ -0,0 +1,132 @@
+import numpy as np
+import cv2
+import pypot.dynamixel
+from simple_pid import PID
+
+pid = PID(4, 0.2, 0.2, setpoint=0)
+
+ports = pypot.dynamixel.get_available_ports()
+# ports = 0
+if not ports:
+    MOTOR_USED = False
+    print("Motor not used.")
+else:
+    MOTOR_USED = True
+    print("Motor used.")
+
+if MOTOR_USED:
+    dxl_io = pypot.dynamixel.DxlIO(ports[0])
+    dxl_io.set_wheel_mode([1])
+    STANDARD_SPEED = 360
+
+video_capture = cv2.VideoCapture(0)
+
+width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+print(f"width:{width}; height:{height}; fps:{fps}")
+
+
+def coord_is_in_left(coord):
+    x, y = coord
+    if y < height / 3:
+        return True
+    return False
+
+
+def coord_is_in_center(coord):
+    x, y = coord
+    if height / 3 <= y and y <= height * 2 / 3:
+        return True
+    return False
+
+
+def coord_is_in_right(coord):
+    x, y = coord
+    if height * 2 / 3 < y:
+        return True
+    return False
+
+
+# To be encoded in BGR
+boundaries = [
+    ([190, 150, 0], [255, 200, 50]),  # A blue tape
+]
+
+
+lower, upper = boundaries[0]
+lower = np.array(lower, dtype="uint8")
+upper = np.array(upper, dtype="uint8")
+
+speed = 0
+
+while True:
+    result, frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    frame = frame[0:height, 590:600]
+
+    # Mask and output for color to be followed
+    mask = cv2.inRange(frame, lower, upper)
+    # output = cv2.bitwise_and(frame, frame, mask=mask)
+    # Find all pixels detected in the mask
+    coords = cv2.findNonZero(mask)
+    # In case if nothing is detected
+    # nb_left = 0
+    nb_center = 0
+    # nb_right = 0
+    if coords is not None:
+        for coord in coords:
+            nb_center += coord[0][1]
+            # # If the coordinate is in the first (left) third
+            # if coord_is_in_left(coord[0]):
+            #     # Show the coordinate as red
+            #     #            output = cv2.circle(output, coord[0], radius=0,
+            #     #                                color=(0, 0, 255), thickness=-1)
+            #     # Add the pixel to the left counter
+            #     nb_left += 1
+            # elif coord_is_in_center(coord[0]):
+            #     #            output = cv2.circle(output, coord[0], radius=0,
+            #     #                                color=(0, 255, 0), thickness=-1)
+            #     nb_center += 1
+            # elif coord_is_in_right(coord[0]):
+            #     #            output = cv2.circle(output, coord[0], radius=0,
+            #     #                                color=(255, 0, 0), thickness=-1)
+            #     nb_right += 1
+        speed = pid(-(nb_center/len(coords))/width + 0.5)
+        # print(nb_center/len(coords)/width - 0.5, ", speed = ", speed)
+
+    else:
+        coords = [0]
+        speed = 0
+        # print("color not detected, speed = ", speed)
+
+    # Percentages of mask pixels in a zone of the image
+    
+
+    if MOTOR_USED:
+        dxl_io.set_moving_speed(
+            {2: -(STANDARD_SPEED + speed*STANDARD_SPEED)})  # Degrees / s
+        dxl_io.set_moving_speed(
+            {1: STANDARD_SPEED - speed*STANDARD_SPEED})  # Degrees / s
+
+    # Visual line, not necessary for computing
+#    cv2.line(output, (0, int(height/3)),
+#             (width, int(height/3)), (255, 0, 0), 2)
+#    cv2.line(output, (0, int(height*2/3)),
+#             (width, int(height*2/3)), (255, 0, 0), 2)
+
+    # Showing images
+    # cv2.imshow("images", np.hstack([frame, output]))
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+if MOTOR_USED:
+    dxl_io.set_moving_speed(
+        {1: 0})  # Degrees / s
+    dxl_io.set_moving_speed(
+        {2: 0})  # Degrees / s
