54a85e78d53e96dd744326c1dd2bb56892cbeed6
Antonin Brauer <antonin.brauer@yahoo.fr>
2025-10-02 09:29:55 +0200

    populated TODOs for go to

--- src/color/control.py
+++ src/color/control.py
@@ -1,6 +1,7 @@
 from math import cos, sin, sqrt, atan2, pi
 from datetime import datetime
 from odom import direct_kinematics, tick_odom
+from pypot import dynamixel 
 
 wheel_distance = 159.40 ## in mm
 
@@ -9,11 +10,23 @@ def inverse_kinematics(linear_speed, angular_speed) -> tuple[float, float]:
     v_gauche = linear_speed - (angular_speed * wheel_distance / 2)
     return (v_droit, v_gauche)
 
+WHEEL_SIZE = 51.
+def rotation_speed_to_linear_speed(rotation_speed) -> float: #in degres/s
+    perimeter = 51*pi
+    return perimeter*rotation_speed/360
+
 DIST_TOLERANCE = 5. # in mm
 ANGLE_TOLERANCE = pi/15. # in radians
 SPEED_RATIO = 0.1
 SPEED_START_ROTATION = 1 ## in mm/s
 def go_to_xya(x, y, theta):
+    ports = dynamixel.get_available_ports()
+    if not ports:
+        exit('No port')
+
+    dxl_io = dynamixel.DxlIO(ports[0])
+    dxl_io.set_wheel_mode([1])
+
     (curr_x, curr_y, curr_theta) = (0., 0., 0.)
     (prev_x, prev_y, prev_theta) = (None, None, None)
     delta_time = 0.
@@ -31,10 +44,16 @@ def go_to_xya(x, y, theta):
             goal_angular_speed += SPEED_RATIO * (theta-curr_theta)
         
         (goal_v_droit, goal_v_gauche) = inverse_kinematics(goal_linear_speed, goal_angular_speed)
+        
         delta_time = start - datetime.now()
+        
+        dxl_io.set_moving_speed({1: goal_v_droit})
+        dxl_io.set_moving_speed({2: goal_v_gauche})
+         
         start = datetime.now()
-        #TODO: Envoyer les commandes au roues 
 
+        real_v_droit =  rotation_speed_to_linear_speed(dxl_io.get_moving_speed(1))
+        real_v_gauche = rotation_speed_to_linear_speed(dxl_io.get_moving_speed(2))
         (real_linear_speed, real_angular_speed) = direct_kinematics(0., 0.) #TODO: Replace zeros by read from motors
         dxw = real_linear_speed*cos(curr_theta+real_angular_speed)
         dyw = real_linear_speed*sin(curr_theta+real_angular_speed)
