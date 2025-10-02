ec92d4f938c991e9b14fc3524252a0c7ad343e15
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-10-02 10:13:01 +0200

    working main and image processing files

---
+++ src/color/image_processing.py
@@ -0,0 +1,241 @@
+import numpy as np
+import cv2
+import pypot.dynamixel
+from simple_pid import PID
+from copy import deepcopy
+
+
+def next_color(dico):
+    boundaries = dico["boundaries"]
+    color_string = dico["color_string"]
+    if dico["current_color"] < len(boundaries)-2:
+        dico["current_color"] += 1
+    else:
+        dico["current_color"] = 0
+    print(color_string[dico["current_color"]])
+    dico["lower"], dico["upper"] = dico["boundaries"][dico["current_color"]]
+
+
+def coord_is_in_left(coord, width):
+    x, y = coord
+    if x < width / 3:
+        return True
+    return False
+
+
+def coord_is_in_center(coord, width):
+    x, y = coord
+    if width / 3 <= x and x <= width * 2 / 3:
+        return True
+    return False
+
+
+def coord_is_in_right(coord, width):
+    x, y = coord
+    if width * 2 / 3 < x:
+        return True
+    return False
+
+
+def process_frame_hsv(frame, dico):
+    if dico["COMPUTER_USED"]:
+        full_frame = deepcopy(frame)
+
+    # frame = frame[0:height, int(width/2)-5:int(width/2)+5]
+    if not dico["COMPUTER_USED"]:
+        frame = frame[dico["top_band"]:dico["bot_band"], 0:dico["width"]]
+
+    print(frame.shape)
+    # Transform from RGB to HSV
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Mask and output for color to be followed
+    mask = cv2.inRange(hsv_frame, dico["lower"], dico["upper"])
+    if dico["BROWN_USED"]:
+        brown_mask = cv2.inRange(
+            hsv_frame, dico["brown_lower"], dico["brown_upper"])
+    if dico["COMPUTER_USED"]:
+        output = cv2.bitwise_and(hsv_frame, hsv_frame, mask=mask)
+        if dico["BROWN_USED"]:
+            brown_output = cv2.bitwise_and(
+                hsv_frame, hsv_frame, mask=brown_mask)
+    # Find all pixels detected in the mask
+    coords = cv2.findNonZero(mask)
+    if dico["BROWN_USED"]:
+        brown_coords = cv2.findNonZero(brown_mask)
+    nb_center = 0
+    # In case if nothing is detected
+    color_detected = True
+    if coords is not None:
+        color_detected = True
+        for coord in coords:
+            nb_center += coord[0][0]
+            if dico["COMPUTER_USED"]:
+                output = cv2.circle(output, coord[0], radius=0,
+                                    color=(0, 0, 255), thickness=-1)
+        # print(nb_center/len(coords)/width - 0.5, ", speed = ", speed)
+
+    else:
+        coords = [0]
+        # print("color not detected, speed = ", speed)
+
+    # Mask and output for color to be followed
+    if dico["BROWN_USED"]:
+        brown_mask = cv2.inRange(
+            hsv_frame, dico["brown_lower"], dico["brown_upper"])
+        if dico["COMPUTER_USED"]:
+            brown_output = cv2.bitwise_and(
+                hsv_frame, hsv_frame, mask=brown_mask)
+
+        brown_nb_left = 0
+        brown_nb_center = 0
+        brown_nb_right = 0
+        if brown_coords is not None:
+            for coord in brown_coords:
+                # If the coordinate is in the first (left) third
+                if coord_is_in_left(coord[0], dico["width"]):
+                    # Show the coordinate as red
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    # Add the pixel to the left counter
+                    brown_nb_left += 1
+                elif coord_is_in_center(coord[0], dico["width"]):
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    brown_nb_center += 1
+                elif coord_is_in_right(coord[0], dico["width"]):
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    brown_nb_right += 1
+        else:
+            brown_coords = [0]
+        brown_center_percentage = brown_nb_center/len(brown_coords)
+        if not dico["switch_ready"] and brown_center_percentage < 0.2:
+            dico["switch_ready"] = True
+        if dico["switch_ready"] and 0.7 <= brown_center_percentage and brown_center_percentage <= 1:
+            next_color(dico)
+            dico["lower"] = np.array(dico["lower"], dtype="uint8")
+            dico["upper"] = np.array(dico["upper"], dtype="uint8")
+            switch_ready = False
+
+    if dico["COMPUTER_USED"]:
+        # Visual line, not necessary for computing
+        cv2.rectangle(output, (int(dico["width"]/3), dico["bot_band"]),
+                      (int(dico["width"]*2/3), dico["top_band"]), (255, 0,  0), 2)
+
+        # cv2.rectangle(output, (int(width/2)-5, 0),
+        #              (int(width/2)+5, height), (0, 255, 0), 2)
+
+        # Showing images
+        cv2.imshow("images", np.hstack([frame, output]))
+
+    if cv2.waitKey(1) & 0xFE == ord("n"):
+        next_color(dico)
+        dico["lower"] = np.array(dico["lower"], dtype="uint8")
+        dico["upper"] = np.array(dico["upper"], dtype="uint8")
+
+    return ((nb_center/len(coords)), color_detected)
+
+
+def process_frame_rgb(frame, dico):
+    if dico["COMPUTER_USED"]:
+        full_frame = deepcopy(frame)
+
+    # frame = frame[0:height, int(width/2)-5:int(width/2)+5]
+    if not dico["COMPUTER_USED"]:
+        frame = frame[dico["top_band"]:dico["bot_band"], 0:dico["width"]]
+
+    # Mask and output for color to be followed
+    mask = cv2.inRange(frame, dico["lower"], dico["upper"])
+    if dico["BROWN_USED"]:
+        brown_mask = cv2.inRange(
+            frame, dico["brown_lower"], dico["brown_upper"])
+    if dico["COMPUTER_USED"]:
+        output = cv2.bitwise_and(frame, frame, mask=mask)
+        if dico["BROWN_USED"]:
+            brown_output = cv2.bitwise_and(
+                frame, frame, mask=brown_mask)
+    # Find all pixels detected in the mask
+    coords = cv2.findNonZero(mask)
+    if dico["BROWN_USED"]:
+        brown_coords = cv2.findNonZero(brown_mask)
+    nb_center = 0
+    # In case if nothing is detected
+    color_detected = True
+    if coords is not None:
+        color_detected = True
+        for coord in coords:
+            nb_center += coord[0][0]
+            if dico["COMPUTER_USED"]:
+                output = cv2.circle(output, coord[0], radius=0,
+                                    color=(0, 0, 255), thickness=-1)
+        # print(nb_center/len(coords)/width - 0.5, ", speed = ", speed)
+
+    else:
+        coords = [0]
+        # print("color not detected, speed = ", speed)
+
+    # Mask and output for color to be followed
+    if dico["BROWN_USED"]:
+        brown_mask = cv2.inRange(
+            frame, dico["brown_lower"], dico["brown_upper"])
+        if dico["COMPUTER_USED"]:
+            brown_output = cv2.bitwise_and(
+                frame, frame, mask=brown_mask)
+
+        brown_nb_left = 0
+        brown_nb_center = 0
+        brown_nb_right = 0
+        if brown_coords is not None:
+            for coord in brown_coords:
+                # If the coordinate is in the first (left) third
+                if coord_is_in_left(coord[0], dico["width"]):
+                    # Show the coordinate as red
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    # Add the pixel to the left counter
+                    brown_nb_left += 1
+                elif coord_is_in_center(coord[0], dico["width"]):
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    brown_nb_center += 1
+                elif coord_is_in_right(coord[0], dico["width"]):
+                    if dico["COMPUTER_USED"]:
+                        output = cv2.circle(output, coord[0], radius=0,
+                                            color=(255, 255, 255), thickness=-1)
+                    brown_nb_right += 1
+        else:
+            brown_coords = [0]
+        brown_center_percentage = brown_nb_center/len(brown_coords)
+        if not dico["switch_ready"] and dico["brown_center_percentage"] < 0.2:
+            dico["switch_ready"] = True
+        if dico["switch_ready"] and 0.7 <= dico["brown_center_percentage"] and dico["brown_center_percentage"] <= 1:
+            dico["lower"], dico["upper"] = dico["boundaries"][next_color(
+                dico["current_color"], dico["boundaries"], dico["color_string"])]
+            lower = np.array(dico["lower"], dtype="uint8")
+            upper = np.array(dico["upper"], dtype="uint8")
+            switch_ready = False
+
+    if dico["COMPUTER_USED"]:
+        # Visual line, not necessary for computing
+        cv2.rectangle(output, (int(dico["width"]/3), dico["bot_band"]),
+                      (int(dico["width"]*2/3), dico["top_band"]), (255, 0,  0), 2)
+
+        # cv2.rectangle(output, (int(width/2)-5, 0),
+        #              (int(width/2)+5, height), (0, 255, 0), 2)
+
+        # Showing images
+        cv2.imshow("images", np.hstack([frame, output]))
+
+    if cv2.waitKey(1) & 0xFE == ord("n"):
+        dico["lower"], dico["upper"] = dico["boundaries"][next_color(
+            dico["current_color"], dico["boundaries"], dico["color_string"])]
+        dico["lower"] = np.array(dico["lower"], dtype="uint8")
+        dico["upper"] = np.array(dico["upper"], dtype="uint8")
+
+    return ((nb_center/len(coords)), color_detected)
---
+++ src/color/main.py
@@ -0,0 +1,164 @@
+import numpy as np
+import cv2
+import pypot.dynamixel
+from simple_pid import PID
+from copy import deepcopy
+import argparse
+import sys
+
+from image_processing import next_color, process_frame_hsv, process_frame_rgb
+
+parser = argparse.ArgumentParser(
+    prog='Main file',
+    description='To launch the robot movements')
+parser.add_argument('-c', '--computer_used', action='store_true',
+                    help='Defines if the program is launched on a computer.')
+parser.add_argument('-m', '--motor_used', action='store_true',
+                    help='Defines if the motors needs to be used.')
+parser.add_argument('-b', '--brown_detection', action='store_true',
+                    help='Defines if the motors needs to be used.')
+parser.add_argument('-r', '--rgb_used', action='store_true',
+                    help='Defines if the RGB is used over the HSV.')
+
+parser.add_argument('-col', '--color', nargs=1,
+                    default=0, type=int,
+                    help='Defines the color of the line to follow.')
+parser.add_argument('-s', '--speed', nargs=1,
+                    default=360, type=float,
+                    help='Defines the standard speed of the wheels. Default is 360.')
+
+args = parser.parse_args()
+
+MOTOR_USED = args.motor_used
+COMPUTER_USED = args.computer_used
+BROWN_USED = args.brown_detection
+RGB_USED = args.rgb_used
+
+print(
+    f"Motor used:{MOTOR_USED}; Computer used:{COMPUTER_USED}; Brown used:{BROWN_USED}; Hsv used:{RGB_USED}")
+
+# Coded in HSV
+# Maroon has to be the last color
+hsv_boundaries = [
+    ([90, 120, 200], [110, 255, 255]),  # A blue tape
+    ([10, 90, 210], [40, 135, 255]),  # A yellow tape (to be reworked)
+    ([110, 100, 200], [180, 200, 255]),  # A red tape
+    ([0, 0, 60], [180, 90, 140])  # A maroon tape (to be reworked)
+
+]
+# Coded in BGR
+# Maroon has to be the last color
+rgb_boundaries = [
+    ([190, 150, 0], [255, 200, 50]),  # A blue tape
+    ([100, 170, 150], [150, 220, 200]),  # A yellow tape (to be reworked on)
+    ([140, 70, 130], [100, 100, 255]),  # A red tape (to be reworked on)
+    ([110, 90, 70], [130, 110, 90]),  # A brown tape (to be reworked on)
+]
+
+color_string = ["Blue Tape", "Yellow Tape",  "Red Tape", "Maroon Tape"]
+current_color = args.color
+
+if RGB_USED:
+    boundaries = rgb_boundaries
+else:
+    boundaries = hsv_boundaries
+
+lower, upper = boundaries[current_color]
+print(color_string[current_color])
+lower = np.array(lower, dtype="uint8")
+upper = np.array(upper, dtype="uint8")
+brown_lower, brown_upper = boundaries[-1]
+brown_lower = np.array(brown_lower, dtype="uint8")
+brown_upper = np.array(brown_upper, dtype="uint8")
+switch_ready = True
+
+
+top_band = 400
+bot_band = 410
+
+
+if MOTOR_USED:
+    ports = pypot.dynamixel.get_available_ports()
+    if not ports:
+        sys.exit("Motors are used but are not detected. Exiting...")
+    else:
+        dxl_io = pypot.dynamixel.DxlIO(ports[0])
+        dxl_io.set_wheel_mode([1])
+        STANDARD_SPEED = 360
+        print(
+            f"Motors detected and used. Setting standard speed at {STANDARD_SPEED}.")
+
+
+video_capture = cv2.VideoCapture(0)
+
+width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(video_capture.get(cv2.CAP_PROP_FPS))
+print(f"width:{width}; height:{height}; fps:{fps}")
+
+pid = PID(1, 0.2, 0.1, setpoint=0)
+
+
+def exit_program():
+    video_capture.release()
+    cv2.destroyAllWindows()
+    if MOTOR_USED:
+        dxl_io.set_moving_speed(
+            {1: 0})  # Degrees / s
+        dxl_io.set_moving_speed(
+            {2: 0})  # Degrees / s
+    sys.exit()
+
+
+try:
+    dico = {
+        "width": width,
+        "height": height,
+        "fps": fps,
+        "top_band": top_band,
+        "bot_band": bot_band,
+        "MOTOR_USED": MOTOR_USED,
+        "COMPUTER_USED": COMPUTER_USED,
+        "BROWN_USED": BROWN_USED,
+        "RGB_USED": RGB_USED,
+        "boundaries": boundaries,
+        "lower": lower,
+        "upper": upper,
+        "brown_lower": brown_lower,
+        "brown_upper": brown_upper,
+        "current_color": current_color,
+        "color_string": color_string,
+        "switch_ready": switch_ready
+    }
+    speed = 0
+    while True:
+        result, frame = video_capture.read()  # read frames from the video
+        if result is False:
+            print("Capture has failed. Exiting...")
+            exit_program()
+
+        if RGB_USED:
+            absisse, color_detected = process_frame_rgb(frame, dico)
+        else:
+            absisse, color_detected = process_frame_hsv(frame, dico)
+
+        if color_detected:
+            speed = pid((absisse-width/2)/(width/2))
+        else:
+            speed = 0
+
+        # print(round(absisse, 2))
+
+        if MOTOR_USED:
+            dxl_io.set_moving_speed(
+                {1: -(STANDARD_SPEED + speed*STANDARD_SPEED)})  # Degrees / s
+            dxl_io.set_moving_speed(
+                {2: STANDARD_SPEED - speed*STANDARD_SPEED})  # Degrees / s
+
+        if COMPUTER_USED:
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                exit_program()
+
+except KeyboardInterrupt:
+    print("KeyboardInterrupt. Exiting...")
+    exit_program()
